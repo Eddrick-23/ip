c9d595c47d82007a5cc941990c4fcb3714333a37
Eddrick <eddricklivando@gmail.com>
2026-08-15 21:17:14 +0800

    Add support for list adding

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    public static class ToDoList {
+        private ArrayList<String> entries;
+
+        public ToDoList() {
+            this.entries = new ArrayList<>();
+        }
+
+        @Override
+        public String toString() {
+            String output = "";
+
+            int count = 1;
+            for (String entry : entries) {
+                output += String.format("%s. %s\n", count++, entry);
+            }
+
+            return output;
+        }
+
+        public void add(String entry) {
+            entries.add(entry);
+        }
+    }
     public static void main(String[] args) {
         String banner = "#   #  #####  #####  #    \n"
                 + "##  #  #        #    #    \n"
@@ -22,13 +46,27 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         String input = "";
 
+        ToDoList toDoList = new ToDoList();
         System.out.print(welcomeMessage);
 
         // main loop, simply echoes user input with dividers
         // "bye" exits the loop.
-        while (!input.equalsIgnoreCase("bye")) {
+        while (true) {
             input = scanner.nextLine();
-            System.out.println(input);
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.print(divider);
+                break;
+            }
+
+            switch (input) {
+                case "list":
+                    System.out.print(toDoList);
+                    break;
+                default:
+                    System.out.println("added: " + input);
+                    toDoList.add(input);
+            }
             System.out.print(divider);
         }
 
