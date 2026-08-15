1cf1cc3e3dca75498b9a7a3c3290ce25b60eb54a
Eddrick <eddricklivando@gmail.com>
2026-08-15 20:48:37 +0800

    Add support for echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String banner =
-                  "#   #  #####  #####  #    \n"
+        String banner = "#   #  #####  #####  #    \n"
                 + "##  #  #        #    #    \n"
                 + "# # #  ####     #    #    \n"
                 + "#  ##  #        #    #    \n"
                 + "#   #  #####  #####  #####\n";
 
         String chatbotName = "Neil";
-        String output = String.format(
-                "____________________________________________________________\n" +
-                "%s\n" +
-                "Hello! I'm %s.\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n", banner, chatbotName);
+        String divider = "____________________________________________________________\n";
+        String byeMessage = "Bye. Hope to see you again soon!\n";
+        String welcomeMessage = String.format(
+                divider +
+                        "%s\n" +
+                        "Hello! I'm %s.\n" +
+                        "What can I do for you?\n" +
+                        divider, banner, chatbotName
+        );
 
-        System.out.println(output);
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        System.out.print(welcomeMessage);
+
+        // main loop, simply echoes user input with dividers
+        // "bye" exits the loop.
+        while (!input.equalsIgnoreCase("bye")) {
+            input = scanner.nextLine();
+            System.out.println(input);
+            System.out.print(divider);
+        }
+
+        System.out.print(byeMessage + divider);
+        scanner.close();
 
     }
 }
