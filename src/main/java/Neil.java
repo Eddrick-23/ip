4c5988daed9fda1ab2c657fb5bdd7b52eae7983f
Eddrick <eddricklivando@gmail.com>
2026-08-23 19:55:20 +0800

    Add Ui class to control printing

--- src/main/java/Neil.java
+++ src/main/java/Neil.java
@@ -2,27 +2,8 @@ import java.util.Scanner;
 
 public class Neil {
     private static Storage storage = new Storage("./data/neil.txt");
+    private static Ui ui = new Ui();
     public static void main(String[] args) {
-        String banner = "#   #  #####  #####  #    \n"
-                + "##  #  #        #    #    \n"
-                + "# # #  ####     #    #    \n"
-                + "#  ##  #        #    #    \n"
-                + "#   #  #####  #####  #####\n";
-
-        String chatbotName = "Neil";
-        String divider = "____________________________________________________________\n";
-        String byeMessage = "Bye. Hope to see you again soon!\n";
-        String welcomeMessage = String.format(
-                divider +
-                        "%s\n" +
-                        "Hello! I'm %s.\n" +
-                        "What can I do for you?\n" +
-                        divider, banner, chatbotName
-        );
-
-        Scanner scanner = new Scanner(System.in);
-        String input = "";
-
         ToDoList toDoList = new ToDoList();
 
         try {
@@ -34,15 +15,13 @@ public class Neil {
             return;
         }
 
-        System.out.print(welcomeMessage);
-
-        // main loop, simply echoes user input with dividers
+        ui.showWelcome();
+        // main loop
         // "bye" exits the loop.
         while (true) {
-            input = scanner.nextLine();
+            String input = ui.readCommand();
 
             if (input.equalsIgnoreCase("bye")) {
-                System.out.print(divider);
                 break;
             }
 
@@ -53,46 +32,49 @@ public class Neil {
                         int taskNumber = Parser.parseTaskNumber(parts);
                         Task task = toDoList.markTaskAsDone(taskNumber);
                         storage.save(toDoList.getTasks());
-                        System.out.println("Nice! I've marked this task as done:");
-                        System.out.println(task);
+                        ui.showMessage(String.format("Nice! I've marked this task as done:\n%s", task));
                         break;
                     }
                     case "unmark": {
                         int taskNumber = Parser.parseTaskNumber(parts);
                         Task task= toDoList.unmarkTask(taskNumber);
                         storage.save(toDoList.getTasks());
-                        System.out.println("OK, I've marked this task as not done yet:");
-                        System.out.println(task);
+                        ui.showMessage(String.format("OK, I've marked this task as not done yet:\n%s", task));
                         break;
                     }
                     case "delete": {
                         int taskNumber = Parser.parseTaskNumber(parts);
                         Task task = toDoList.remove(taskNumber);
                         storage.save(toDoList.getTasks());
-                        System.out.println("Noted. I've removed this task:");
-                        System.out.println(task);
-                        System.out.println("Now you have " + toDoList.size() + " tasks in the list.");
+                        String msg = String.format(
+                                "Noted. I've removed this task:\n%s\n Now you have %d tasks in this list.",
+                                task,
+                                toDoList.size()
+                        );
+                        ui.showMessage(msg);
                         break;
                     }
-                    case "list":
-                        System.out.println("Here are the tasks in your list:");
-                        System.out.print(toDoList);
+                    case "list": {
+                        String msg = String.format("Here are the tasks in your list:\n%s", toDoList);
+                        ui.showMessage(msg);
                         break;
+                    }
                     default:
                         Task task = Parser.parseTask(input);
                         toDoList.add(task);
                         storage.save(toDoList.getTasks());
-                        System.out.println("Got it. I've added this task:\n " + task);
-                        System.out.println("Now you have " + toDoList.size() + " tasks in the list.");
+                        // TODO extra space on the last line?
+                        String msg = String.format("Got it. I've added this task:\n%s\n", task);
+                        msg += "Now you have " + toDoList.size() + " tasks in the list.";
+                        ui.showMessage(msg);
                 }
             } catch (NeilException e) {
                 System.out.println(e.getMessage());
             }
-            System.out.print(divider);
         }
 
-        System.out.print(byeMessage + divider);
-        scanner.close();
+        ui.showGoodbye();
+        ui.close();
 
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+/**
+ * Handles input from and output to the user.
+ */
+public class Ui {
+    private static final String DIVIDER =
+            "____________________________________________________________";
+
+    private final Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void showWelcome() {
+        String banner =
+                "#   #  #####  #####  #    \n"
+                        + "##  #  #        #    #    \n"
+                        + "# # #  ####     #    #    \n"
+                        + "#  ##  #        #    #    \n"
+                        + "#   #  #####  #####  #####";
+
+        System.out.println(DIVIDER);
+        System.out.println(banner);
+        System.out.println();
+        System.out.println("Hello! I'm Neil.");
+        System.out.println("What can I do for you?");
+        System.out.println(DIVIDER);
+    }
+
+    public void showGoodbye() {
+        System.out.println(DIVIDER);
+        System.out.println(
+                "Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
+    }
+
+    public void showError(String message) {
+        System.out.println(message);
+        System.out.println(DIVIDER);
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+        System.out.println(DIVIDER);
+    }
+
+    public void close() {
+        scanner.close();
+    }
+}
